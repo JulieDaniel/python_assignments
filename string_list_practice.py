f6357b2303e705acf11cbdbfb98d9a7b1d25dcdf
JulieDaniel <33355156+JulieDaniel@users.noreply.github.com>
2017-11-28 17:31:55 -0800

    string_list_practice assignment uploaded

---
+++ string_list_practice.py
@@ -0,0 +1,23 @@
+words = "It's thanksgiving day. It's my birthday,too!"
+print words.find("day")
+
+print words.replace("day", "month")
+
+x = [2,54,-2,7,12,98]
+print min(x)
+print max(x)
+
+x = ["hello",2,54,-2,7,12,98,"world"]
+print x[0]
+print x[7]
+
+y= [x[0], x[7]]
+print y
+
+x = [19,2,54,-2,7,12,98,32,10,-3,6]
+x.sort()
+print x[-1]
+a=x[0:5]
+b=x[5:]
+c=[a]+ b
+print c
