0e9aa698c40b957eaa205074f8b21460cdaf0e6d
JulieDaniel <33355156+JulieDaniel@users.noreply.github.com>
2017-12-02 13:21:11 -0800

    scores_grades assignment uploaded

---
+++ scores_grades.py
@@ -0,0 +1,16 @@
+def grades():
+    import random
+    for n in range(0, 10, 1):
+        score = random.randint(60, 100)
+        if (score >= 60) and (score <= 69):
+            print "Your score is:", score, "Your grade is a D."
+        elif (score >=70) and (score <= 79):
+            print "Your score is:", score, "Your grade is a C."
+        elif (score >=80) and (score<=89):
+            print "Your score is:", score, "Your grade is a B."
+        elif (score >=90) and (score<=100):
+            print "Your score is:", score, "Your grade is an A."
+        else:
+            print "That's all the grades."
+
+print (grades())
