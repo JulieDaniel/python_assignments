1d99f442495f25404750b2d339a73200107efbf6
JulieDaniel <33355156+JulieDaniel@users.noreply.github.com>
2017-12-04 14:16:38 -0800

    dictionary_basics uploaded

---
+++ dictionary_basics.py
@@ -0,0 +1,9 @@
+myself={
+    'name': 'Julie',
+    'age': '38',
+    'country of birth': 'United States',
+    'favorite language': 'Python'
+}
+
+for key, data in myself.items():
+    print ("My "+ key + " is "+ str(data))
